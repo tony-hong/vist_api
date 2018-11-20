01ddf66104d6c2c160c2e7753ebc839846147938
Tony Hong <humanhong1001@gmail.com>
2018-11-20 18:21:21 +0100

    feat: get raw data

--- test/test.py
+++ test/test.py
@@ -2,6 +2,6 @@ import sys
 sys.path.insert(0, '../')
 import vist
 
-sis = vist.Story_in_Sequence('/playpen/data/vist')
+sis = vist.Story_in_Sequence('', '/Users/tonyhong/ROOT/coli/SIND/data')
 story_id = sis.Stories.keys()[0]
 print sis.Stories[story_id]
